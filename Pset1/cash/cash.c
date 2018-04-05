1c449966cc03d8f743a70ac2a2102e380f1bcfac
Michael A Nowiszewski <micski@gmail.com>
2018-04-05 02:32:42 +0000

    add ca.c

--- Pset1/cash/cash.c
+++ Pset1/cash/cash.c
@@ -1,1 +1,28 @@
 // how much change , minimum number of coins
+//How to eat an elephant? One chunk at a time - refactored using modulo
+#include < stdio.h >
+
+  int main(void) {
+    float dollars;
+    do {
+      dollars = get_float("Change: ");
+    }
+    while (dollars < 0);
+
+    int cents = round(dollars * 100);
+
+    int coins = 0;
+//quarters
+    coins += cents / 25;
+
+    cents = cents % 25;
+//dimes
+    coins += cents / 10;
+    cents = cents % 10;
+//nickels
+    coins += cents / 5;
+    cents = cents % 5;
+//pennies
+    coins += cents;
+
+    printf("%i\n", coins);
